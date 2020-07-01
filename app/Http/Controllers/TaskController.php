062302cbc059cb3694d0a4494292a97d8a1a9d41
raju1705 <rajucse1705@gmail.com>
2020-07-01 14:51:39 +0600

    apis

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+     public function index()
+    {
+        $task = Task::all();
+        // dd(count($task));
+        if (count($task) == 0) {
+            return response('no content',204);
+        }
+
+        return response($task,200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|max:500|string',
+            'isComplete' => 'boolean',
+        ]);
+
+        $task = Task::create($validatedData);
+
+        return response($task,201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        return response($task,200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        $validatedData = $request->validate([
+            'title' => 'min:1|max:500|string',
+            'isComplete' => 'boolean',
+        ]);
+
+        $task->update($validatedData);
+
+        return response($task,200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response($task,200);
+
+    }
+}
